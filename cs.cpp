77018f85f5314ee71640f46dc54ab2cd481031eb
sachinsandrons2005 <125856219+sachinsandrons2005@users.noreply.github.com>
2023-02-20 12:05:33 +0530

    Add files via upload

---
+++ cs.cpp
@@ -0,0 +1,74 @@
+/*#include<stdio.h>
+int main()
+{
+	int c;
+	scanf("%d",&c);
+	switch(c)
+	{
+	
+		case 1:
+		{
+			
+			printf("one");
+			break;
+		}
+	
+	
+	    case 2:
+		{
+			printf("two");
+			break;
+		}
+	
+	
+		case 3:
+		{
+			
+			printf("three");
+			break;
+		}
+	
+	
+		case 4:
+		{
+				
+			printf("four");
+			break;
+	   }
+	
+
+		case 5:
+		{
+			
+			printf("five");
+			break;
+		}
+	
+	
+		default :
+			printf("invalid input");
+	}
+	
+	
+	
+	
+}*/
+/*#include<stdio.h>
+int main()
+{
+	int i;
+	scanf("%d",&i);
+	for(i=1;i<=10;i++)
+	{
+		printf("%d",i);
+	}
+}*/
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=10;i>0;i++)
+	{
+		printf("%d ",i);
+	}
+}
